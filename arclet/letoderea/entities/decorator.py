0e89b817c1c3297caf6f4eb4d3d26321aa66a404
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2021-11-21 17:11:46 +0800

    0.1.2

--- arclet/letoderea/entities/decorator.py
+++ arclet/letoderea/entities/decorator.py
@@ -1,56 +1,68 @@
-from typing import Type, Optional, Any, overload, List
+from typing import Any, overload, List, Optional
 from ..utils import ArgumentPackage
+from abc import ABC, abstractmethod
 
 
-class TemplateDecorator:
+class TemplateDecorator(ABC):
     """
     参数装饰器基类，用于修饰函数或事件
 
     Args:
-        target_type: 目标参数的类型
-        supplement_type: 参数被装饰后的类型,通常与target_type一致
         keep: 是否保留未装饰的参数,若为False则会覆盖原有参数
     """
-    target_type: Type
-    supplement_type: Type
+
+    __disable__: List[str] = ""
+
     keep: bool = True
+    may_target_type: type
 
-    __disable__: List[str]
+    def __init__(self, target_type: Optional[type] = None, keep: bool = True):
+        """
+            参数装饰器基类，用于修饰函数或事件
 
-    def __init__(self, target_type: Type, supplement_type: Optional[Type] = None, keep: bool = True):
-        self.target_type = target_type
-        self.supplement_type = supplement_type or target_type
+            Args:
+                target_type: 可能的目标参数类型,若填入则装饰器只接受该类型的参数
+                keep: 是否保留未装饰的参数,若为False则会覆盖原有参数
+            """
+        self.may_target_type = target_type
         self.keep = keep
 
+    def supply_wrapper(self, args_name: str, args_value: Any):
+        result = self.supply(ArgumentPackage(args_name, args_value.__class__, args_value))
+        if result is None:
+            return
+        if not self.keep:
+            return {args_name: result}
+        return {result.__class__.__name__: result}
+
     @staticmethod
+    @abstractmethod
     @overload
-    def before_parser(target_argument: ArgumentPackage) -> Any:
+    def supply(target_argument: ArgumentPackage) -> Any:
         ...
 
-    def before_parser(self, target_argument: ArgumentPackage) -> Any:
-        """生命周期钩子: 在回调函数的参数被解析前被调用, 用于修饰事件的参数
+    @abstractmethod
+    def supply(self, target_argument: ArgumentPackage) -> Any:
+        """在回调函数的参数被解析后调用, 用于修饰函数的参数
 
         Args:
             target_argument: 一个保存了参数名、参数注解与参数内容的类
         Returns:
-            装饰后的值,该值的类型应与supplement_type一致
+            装饰后的值
         """
+
+
+class EventDecorator(TemplateDecorator):
+    __disable__ = 'after_parser'
+
+    @abstractmethod
+    def supply(self, target_argument: ArgumentPackage) -> Any:
         return target_argument.value
 
-    @staticmethod
-    @overload
-    def after_parser(target_argument: ArgumentPackage) -> Any:
-        ...
 
-    def after_parser(self, target_argument: ArgumentPackage) -> Any:
-        """生命周期钩子: 在回调函数的参数被解析后被调用, 用于修饰函数的参数
+class ArgsDecorator(TemplateDecorator):
+    __disable__ = 'before_parser'
 
-        Args:
-            target_argument: 一个保存了参数名、参数注解与参数内容的类
-        Returns:
-            装饰后的值,该值的类型应与supplement_type一致
-        """
+    @abstractmethod
+    def supply(self, target_argument: ArgumentPackage) -> Any:
         return target_argument.value
-
-    def __eq__(self, other: "TemplateDecorator"):
-        return all([self.target_type == other.target_type, self.supplement_type == other.supplement_type])
