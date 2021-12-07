23e39f3af8914b6c23ea7f084e734a212f67ddce
Tarrailt <42648639+RF-Tar-Railt@users.noreply.github.com>
2021-12-07 23:22:39 +0800

    0.2.3

--- arclet/letoderea/entities/condition.py
+++ arclet/letoderea/entities/condition.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import overload, Callable
+from .event import TemplateEvent
 
 
 class TemplateCondition(ABC):
@@ -11,17 +12,17 @@ class TemplateCondition(ABC):
     @staticmethod
     @overload
     @abstractmethod
-    def judge() -> bool:
+    def judge(*args, **kwargs) -> bool:
         ...
 
     @classmethod
     @overload
     @abstractmethod
-    def judge(cls) -> bool:
+    def judge(cls, *args, **kwargs) -> bool:
         ...
 
     @abstractmethod
-    def judge(self) -> bool:
+    def judge(self, *args, **kwargs) -> bool:
         """外部方法,用于判断该条件是否成立并返回结果; 该方法可以是 `stasticmethod`,`classmethod` 亦或是普通的方法/函数.
         唯一的要求是返回值必须为一布尔值
         """
@@ -29,3 +30,21 @@ class TemplateCondition(ABC):
 
     def __eq__(self, other: "TemplateCondition"):
         return self.__annotations__ == other.__annotations__
+
+
+class EventCondition(TemplateCondition):
+
+    @staticmethod
+    @overload
+    @abstractmethod
+    def judge(event: TemplateEvent) -> bool:
+        ...
+
+    @classmethod
+    @overload
+    @abstractmethod
+    def judge(cls, event: TemplateEvent) -> bool:
+        ...
+
+    def judge(self, event: TemplateEvent) -> bool:
+        pass
